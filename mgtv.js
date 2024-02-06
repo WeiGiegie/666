b1d51806e3f3a1b84348010fa4d768f218a2dae7
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-02-06 23:19:47 +0800

    Update mgtv.js

--- mgtv.js
+++ mgtv.js
@@ -15,7 +15,7 @@
 # > 芒果TV,Vip+会员画质+去广告,页面优化
 ^https?:\/\/.*mgtv.com.*(GetUserInfo|module\/list\?_support|app\/bdboot|pullReleaseInfo|floor\/detail|video\/source|entry\?_support).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/mgtv.js
 ^https?:\/\/mobile-stream\.api\.mgtv\.com\/v1\/video\/source\? url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mgtk.js
-^https？://mobile.da.mgtv.com\/m\/page url reject
+^https?://mobile.da.mgtv.com\/m\/page url reject
 
 [mitm] 
 hostname = *mgtv.com

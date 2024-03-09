980a20b63d6799992d1cf6d4259810994c115cbc
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-03-09 15:56:30 +0800

    Update slzd.js

--- slzd.js
+++ slzd.js
@@ -5,7 +5,7 @@
 软件版本：++
 下载地址：苹果商店下载
 脚本作者：
-更新时间：2024年3月2日 08:43
+更新时间：2024年3月9日 15:54
 电报频道：
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
@@ -34,22 +34,22 @@ if (url.indexOf(p1) != -1) {
         },
         "model": {
             "msgRed": 0,
-            "registerTime": 1709340232749,
+            "registerTime": 1709970876504,
             "arOpen": true,
             "visitor": {
-                "uid": 2695096,
-                "ticket": "1709340232764tlybp"
+                "uid": 2698391,
+                "ticket": "1709970876518ngkpp"
             },
             "buyZhuanlanNewArticle": 1,
             "buyMsgNum": 1,
             "userInfo": {
                 "phone": "",
-                "uid": 2695096,
+                "uid": 2698391,
                 "userVflag": 0,
                 "remark": "此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖!!!作者TG频道 : https://t.me/GieGie777",
                 "pic": "https://zdimg.lifeweek.com.cn/app/20230410/16811146599505136.jpg",
                 "isVip": true,
-                "name": "by-WR1"
+                "name": "by-WR6"
             }
         },
         "resultMsg": "操作成功！",
@@ -75,21 +75,21 @@ if (url.indexOf(p2) != -1) {
             "vipLevel": 4,
             "vipExpiration": "2999-09-09",
             "vip": true,
-            "username": "by-WR1",
+            "username": "by-WR6",
             "vipValid": true
         }, {
             "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
             "vipLevel": 5,
             "vipExpiration": "",
             "vip": false,
-            "username": "by-WR1",
+            "username": "by-WR6",
             "vipValid": true
         }, {
             "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
             "vipLevel": 6,
             "vipExpiration": "2999-09-09",
             "vip": true,
-            "username": "by-WR1",
+            "username": "by-WR6",
             "vipValid": true
         }],
         "resultMsg": "操作成功！",

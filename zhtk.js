b401af6acbe39ae599832c815aabd169ed88dff6
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-03-09 20:59:57 +0800

    Update zhtk.js

--- zhtk.js
+++ zhtk.js
@@ -1,4 +1,3 @@
-
 /*
  *
  *
@@ -13,7 +12,7 @@
 *******************************
 [rewrite_local]
 # > 知乎vip 盐选内容，
-^https?:\/\/(api|app|www).zhihu.com\/(unlimited\/go\/my_card|people\/self|bazaar\/vip_tab|sku\/reversion_sku).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/zhvip.js
+^https?:\/\/((api|app|www).zhihu.com\/|.*resolv\?host)(unlimited\/go\/my_card|people\/self|bazaar\/vip_tab|sku\/reversion_sku|.*zhihu).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/zhvip.js
 ^https?:\/\/((www)|(api)).zhihu.com\/(api\/v3\/books|market\/paid_magazine|market\/paid_column|market\/album|remix\/resource\/paid_magazine|appview\/v2\/answer).*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/zhtk.js
 
 ^https?://api\.zhihu\.com/commercial_api/launch_v2 url reject

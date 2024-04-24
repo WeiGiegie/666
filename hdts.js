2a200ca25816bb2b18729b8da774bf9e02f1382e
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-04-24 14:33:31 +0800

    Update hdts.js

--- hdts.js
+++ hdts.js
@@ -1,7 +1,7 @@
 /*
  *
  *
-脚本功能：红豆听书 解锁付费听书购买下载⏬
+脚本功能：忙豆听书 解锁付费听书购买下载⏬
 软件版本：
 下载地址：
 脚本作者：**
@@ -11,7 +11,7 @@
 使用声明：⚠️此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
-# > 红豆听书 解锁付费听书购买
+# > 忙豆听书 解锁付费听书购买
 ^https?:\/\/md.liaoliaoy.com\/listen\/api\/getUserInfo url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/hdts.js
 ^https?:\/\/md.liaoliaoy.com\/listen\/api\/(buyBookInit|buyCard|batchBuyChapter|bookChapter|getNextChapter).*$ url script-request-body https://raw.githubusercontent.com/WeiGiegie/666/main/hdtsck.js
 # > 去广告

ef7a4ed30ed811fbfbddcf9d305d7316b560037e
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-05-07 19:14:01 +0800

    更新 byhz.js

--- byhz.js
+++ byhz.js
@@ -12,7 +12,8 @@
 *******************************
 [rewrite_local]
 # >鲍鱼盒子18+(不显示会员但是直接能看)
-^https?:\/\/.+\/api(\/video\/report_item?|\/live\/room\/detail?|\/video\/related?|\/video\/detail|\/social\/posts_info).*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/byhz.js
+^https?:\/\/.+\/api\/(video\/report_item?|live\/room\/detail?|video\/related?|video\/detail|socialposts_info).*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/byhz.js
+
 
 [mitm] 
 hostname = 103.39.222.193,yubaoyu.oss-cn-shenzhen.aliyuncs.com,*api.h8h4h.com,42.157.129.25

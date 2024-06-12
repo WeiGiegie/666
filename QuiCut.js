024bd4c21673431b0f857c5a901984ec47413a35
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-06-12 22:11:55 +0800

    更新 QuiCut.js

---
+++ QuiCut.js
@@ -0,0 +1,31 @@
+/*
+ *
+ *
+脚本功能：quicut-video editor & maker（永久会员）
+软件版本：1.0.1
+下载地址：
+脚本作者：
+更新时间：2024年
+电报频道：https://t.me/GieGie777
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# >quicut-video editor & maker（永久会员）
+^https?:\/\/outcut.szsszykj.com\/ap\/OverseasPay\/GetVip url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/QuiCut.js
+
+[mitm] 
+hostname = outcut.szsszykj.com
+*
+*
+*/
+
+
+
+
+
+
+
+
+
+if($request["\u0075\u0072\u006c"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"]("piVteG/yaPsaesrevO/ipa/moc.jkyzsszs.tuctuo//:ptth".split("").reverse().join(""))){let body=JSON["\u0070\u0061\u0072\u0073\u0065"]($response["\u0062\u006f\u0064\u0079"]);Object["\u0061\u0073\u0073\u0069\u0067\u006e"](body["\u0064\u0061\u0074\u0061"],{"\u0065\u006e\u0064\u005f\u0074\u0069\u006d\u0065":"\u0032\u0039\u0039\u0039\u002d\u0030\u0039\u002d\u0030\u0039"});$done({"\u0062\u006f\u0064\u0079":JSON["\u0073\u0074\u0072\u0069\u006e\u0067\u0069\u0066\u0079"](body)});$done({});}

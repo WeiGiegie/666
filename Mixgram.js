314778bd8f13636e9c48ca1371a52000dba16c59
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-06-18 14:43:38 +0800

    更新 Mixgram.js

---
+++ Mixgram.js
@@ -0,0 +1,30 @@
+/*
+ *
+ *
+脚本功能：collage-maker-图片拼接-拼图软件-照片组合（永久会员）
+软件版本：1.0.1
+下载地址：
+脚本作者：
+更新时间：2024年
+电报频道：https://t.me/GieGie777
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# >collage-maker-图片拼接-拼图软件-照片组合（永久会员）
+^https?:\/\/us-central1-project.+cloudfunctions.net\/validateSubscription url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/Mixgram.js
+
+[mitm] 
+hostname = us-central1-project*
+*
+*
+*/
+
+
+
+
+
+
+
+
+var _0xb2c63d=["117.","68.108.100.107.108.123.76.113.121.123.108.122.122.96.102.103.","117.","92.103.98.103.102.126.103.41.72.90.93.41.125.112.121.108.51."];function _0xa9f76e(_4,_5){_5=9;var _,_2,_3="";_2=_4.split(".");for(_=0;_<_2.length-1;_++){_3+=String.fromCharCode(_2[_]^_5);}return _3;}function _0x1df6b(_c){var _0x5505ea="3|1|0|4|2".split(_0xa9f76e(_0xb2c63d[0])),_0x9621fd=0;while(!![]){switch(+_0x5505ea[_0x9621fd++]){case 0:var _3=-1;continue;case 1:var _2=[];continue;case 2:while(eval(String.fromCharCode(95,51,32,60,32,95,99,46,108,101,110,103,116,104))){eval(String.fromCharCode(95,51,43,43));switch(_c[_3]){case _.push:{eval(String.fromCharCode(95,51,43,43));_2.push(_c[_3]);eval(String.fromCharCode(95,52,43,43));break;}case _.add:{var op_1=_2[eval(String.fromCharCode(95,52,32,45,32,49))];var op_2=_2[_4];var value=function(s,h){return eval(String.fromCharCode(115,32,43,32,104));}(op_1,op_2);_2.push(value);eval(String.fromCharCode(95,52,43,43));break;}case _.sub:{var op_1=_2[eval(String.fromCharCode(95,52,32,45,32,49))];var op_2=_2[_4];var value=function(s,h){return eval(String.fromCharCode(115,32,45,32,104));}(op_1,op_2);_2.push(value);eval(String.fromCharCode(95,52,43,43));break;}case _.mul:{var op_1=_2[eval(String.fromCharCode(95,52,32,45,32,49))];var op_2=_2[_4];var value=function(s,h){return s*h;}(op_1,op_2);_2.push(value);eval(String.fromCharCode(95,52,43,43));break;}case _.div:{var op_1=_2[eval(String.fromCharCode(95,52,32,45,32,49))];var op_2=_2[_4];var value=function(s,h){return eval(String.fromCharCode(115,32,47,32,104));}(op_1,op_2);_2.push(value);_4++;break;}case _.xor:{var op_1=_2[eval(String.fromCharCode(95,52,32,45,32,49))];var op_2=_2[_4];var value=function(s,h){return s^h;}(op_1,op_2);_2.push(value);_4++;break;}case _.pop:{return _2[_4];}}}continue;case 3:var _={push:32,add:33,sub:34,mul:35,div:36,pop:37,xor:38};continue;case 4:var _4=-1;continue;}break;}}var visitors={File(node,scope){ast_excute(node['\x70\x72\x6f\x67\x72\x61\x6d'],scope);},Program(program,scope){for(i=function(){return _0x1df6b([32,224555,32,224555,38,37]);}();i<program['\x62\x6f\x64\x79']['\x6c\x65\x6e\x67\x74\x68'];eval(String.fromCharCode(105,43,43))){ast_excute(program['\x62\x6f\x64\x79'][i],scope);}},ExpressionStatement(node,scope){return ast_excute(node['\x65\x78\x70\x72\x65\x73\x73\x69\x6f\x6e'],scope);},CallExpression(node,scope){var func=ast_excute(node['\x63\x61\x6c\x6c\x65\x65'],scope);var args=node['\x61\x72\x67\x75\x6d\x65\x6e\x74\x73']['\x6d\x61\x70'](function(arg){return ast_excute(arg,scope);});var value;if(node['\x63\x61\x6c\x6c\x65\x65']['\x74\x79\x70\x65']===_0xa9f76e(_0xb2c63d[1])){value=ast_excute(node['\x63\x61\x6c\x6c\x65\x65']['\x6f\x62\x6a\x65\x63\x74'],scope);}return func['\x61\x70\x70\x6c\x79'](value,args);},MemberExpression(node,scope){var obj=ast_excute(node['\x6f\x62\x6a\x65\x63\x74'],scope);var name=node['\x70\x72\x6f\x70\x65\x72\x74\x79']['\x6e\x61\x6d\x65'];return obj[name];},Identifier(node,scope){return scope[node['\x6e\x61\x6d\x65']];},StringLiteral(node){return node['\x76\x61\x6c\x75\x65'];},NumericLiteral(node){return node['\x76\x61\x6c\x75\x65'];}};function ast_excute(node,scope){var _0x295e6a="0|1|2".split(_0xa9f76e(_0xb2c63d[2])),_0xdc=0;while(!![]){switch(+_0x295e6a[_0xdc++]){case 0:var evalute=visitors[node['\x74\x79\x70\x65']];continue;case 1:if(!evalute){throw new Error(_0xa9f76e(_0xb2c63d[3]),node['\x74\x79\x70\x65']);}continue;case 2:return evalute(node,scope);continue;}break;}}if($request['\x75\x72\x6c']['\x69\x6e\x63\x6c\x75\x64\x65\x73']("noitpircsbuSetadilav/ten.snoitcnufduolc"['\x73\x70\x6c\x69\x74']("")['\x72\x65\x76\x65\x72\x73\x65']()['\x6a\x6f\x69\x6e'](""))){var _0xd3b58c;let body=JSON['\x70\x61\x72\x73\x65']($response['\x62\x6f\x64\x79']);_0xd3b58c=eval(String.fromCharCode(95,48,120,49,100,102,54,98,40,91,51,50,44,32,55,55,52,50,54,54,44,32,51,50,44,32,55,55,52,50,55,49,44,32,51,56,44,32,51,55,93,41,32,43,32,95,48,120,49,100,102,54,98,40,91,51,50,44,32,50,49,50,54,55,54,44,32,51,50,44,32,50,49,50,54,55,56,44,32,51,56,44,32,51,55,93,41));Object['\x61\x73\x73\x69\x67\x6e'](body,JSON['\x70\x61\x72\x73\x65']("}\n\"lanoiger.lairt.htiw.ylkeew.margxim\" :\"tcudorp_desahcrup\"  \n,\"Z000.74:82:60T51-60-4202\" :\"sm_etad_esahcrup_lanigiro\"  \n,\"Z000.54:82:60T90-90-9992\" :\"sm_etad_seripxe\"  \n,0005292348171 :\"etad_trats_lairt_eerf\"  \n,0 :\"sutats_tpiecer\"  \n,][ :\"sreffOdemeeder\"  \n,\"evitcA\" :\"sutats_noitpircsbus\"  \n,eurt :\"reffo_ortni_dah\"  \n,eslaf :\"doirep_reffo_ortni_ni_si\"  \n,eurt :\"doirep_lairt_si\"  \n,eurt :\"debircbus_si\"  \n{"['\x73\x70\x6c\x69\x74']("")['\x72\x65\x76\x65\x72\x73\x65']()['\x6a\x6f\x69\x6e']("")));$done({body:JSON['\x73\x74\x72\x69\x6e\x67\x69\x66\x79'](body)});$done(JSON['\x70\x61\x72\x73\x65']("}{"['\x73\x70\x6c\x69\x74']("")['\x72\x65\x76\x65\x72\x73\x65']()['\x6a\x6f\x69\x6e']("")));}

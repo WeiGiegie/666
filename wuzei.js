92e31307d5f1280de45da28a29c14158e8e417ec
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-06-24 11:01:14 +0800

    更新 wuzei.js

--- wuzei.js
+++ wuzei.js
@@ -2,10 +2,10 @@
  *
  *
 脚本功能：污贼直播（前神户）会员+去广告
-软件版本：0.8.0
+软件版本：0.8.0++
 下载地址：
 脚本作者：
-更新时间：2024年6月24日 09:42
+更新时间：2024年,6.20
 电报频道：https://t.me/GieGie777
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
@@ -13,7 +13,7 @@
 [rewrite_local]
 # >18+ 污贼直播（前神户）会员+去广告
 ^https?:\/\/.+Api\/(Account\/Login|App\/Splash|App\/Update|App\/PlatData|App\/homeData|Live\/Play).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/wuzei.js
-^https?:\/\/www.auth.open.52kuajing002.51kuajing.net\/Api/Live\/Play.*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/wztk.js
+^https?:\/\/www\.auth\.open\.52kuajing002\.51kuajing\.net\/Api\/Account\/Login.*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/wztk.js
 
 [mitm] 
 hostname = www.auth.open.52kuajing002.51kuajing.net,*.aliyuncs.com,videocloud.cn-hangzhou.log.aliyuncs.com

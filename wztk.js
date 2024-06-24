f02937c680fe4ec16feb904dd397788861fe7e7e
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-06-24 11:23:11 +0800

    更新 wztk.js

--- wztk.js
+++ wztk.js
@@ -29,7 +29,7 @@ var modifiedHeaders = $request.headers;
 modifiedHeaders['device'] = '{"name":"aVBob25l","systemName":"iOS","systemVersion":"16.2","model":"iPhone","localizedModel":"iPhone","identifierForVendor":"7E55FA15-40B1-4FBD-9B9F-A1465944C171","isPhysicalDevice":true,"bundleID":"game.squid.xy"}' ;
 modifiedHeaders['cookie'] = '' ;
 
-modifiedHeaders['auth'] = '' ;
+modifiedHeaders['auth'] = 'eyJhaWQiOiIxMDAwMTUzMjgiLCJ1c2VyX3Rva2VuIjoiMzMwODRjZjk0MTVmNWQ3ZTA4MjUzMzY3ZjZjNjVlMjMifQ==' ;
 
 $done({headers : modifiedHeaders});
 

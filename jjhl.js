a4703828b424239d03ba15e1205b621447bc1937
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-07-08 19:39:05 +0800

    更新 jjhl.js

---
+++ jjhl.js
@@ -0,0 +1,27 @@
+/*
+ *
+ *
+脚本功能：极简汇率 - 让货币流通更简单（永久会员）
+软件版本：6.0.3
+下载地址：
+脚本作者：
+更新时间：2024年
+电报频道：https://t.me/GieGie777
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# >极简汇率 - 让货币流通更简单（永久会员）
+^https?:\/\/explorer.tratao.com\/api\/client\/xtool\/vip url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/jjhl.js
+
+[mitm] 
+hostname = explorer.tratao.com
+*
+*
+*/
+
+
+
+
+if($request["\u0075\u0072\u006c"]["\u0069\u006e\u0063\u006c\u0075\u0064\u0065\u0073"]("piv/lootx/tneilc/ipa/moc.oatart.rerolpxe//:sptth".split("").reverse().join(""))){let body=JSON["\u0070\u0061\u0072\u0073\u0065"]($response["\u0062\u006f\u0064\u0079"]);Object["\u0061\u0073\u0073\u0069\u0067\u006e"](body["\u0064\u0061\u0074\u0061"][0xce78e^0xce78e],{"\u0076\u0069\u0070\u0053\u0074\u0061\u0074\u0075\u0073":"\u0070\u0061\u0069\u0064","\u0076\u0069\u0070\u004c\u0065\u0076\u0065\u006c":"\u0073\u0065\u006e\u0069\u006f\u0072","\u0065\u0078\u0070\u0069\u0072\u0065":'2999-09-09\x2009:09:09',"\u0076\u0069\u0070\u0050\u0061\u0079\u0054\u0079\u0070\u0065":"\u0061\u0075\u0074\u006f\u005f\u0073\u0075\u0062",'vipPayUnit':'month','vipPayNum':0x1});$done({"\u0062\u006f\u0064\u0079":JSON["\u0073\u0074\u0072\u0069\u006e\u0067\u0069\u0066\u0079"](body)});$done({});}
+

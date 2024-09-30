dfc61b2c7c77e036604325e6fafb280649876ded
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-09-30 13:01:36 +0800

    更新 mkztk.js

--- mkztk.js
+++ mkztk.js
@@ -5,7 +5,7 @@
 软件版本：++
 下载地址：
 脚本作者：
-更新时间：2024年9月16日 09:49
+更新时间：2024年9月30日 13:01
 电报频道：https://t.me/GieGie777
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
@@ -24,8 +24,8 @@ hostname = member.mkzcdn.com,comic.mkzcdn.com,base.mkzcdn.com
 
 
 var modifiedUrl = $request.url
-.replace(/(&sign=\w+)/, '&sign=feed519c9d1d23ca7249cfbc3b2c2272')
-.replace(/(&uid=\d+)/, '&uid=75651511');
+.replace(/(&sign=\w+)/, '&sign=b75c5b48681a341cca5188b647f22eed')
+.replace(/(&uid=\d+)/, '&uid=75950793');
 
 
 $done({

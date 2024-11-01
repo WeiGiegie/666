b4490eb83dad7592e99f63f5eaa5ffca3a5223e6
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2024-11-01 19:12:09 +0800

    更新 wuzeick.js

--- wuzeick.js
+++ wuzeick.js
@@ -1,9 +1,15 @@
-var modifiedHeaders = $request.headers;
-
-modifiedHeaders['device'] = '{"board":"taro","bootloader":"unknown","brand":"OnePlus","device":"OP5913L1","display":"PHK110_11_A.23","fingerprint":"OnePlus/PHK110/OP5913L1:13/SKQ1.221012.001/T.119565e-24c-14506e:user/release-keys","hardware":"qcom","host":"dg02-pool03-kvm97","id":"SKQ1.221012.001","manufacturer":"OnePlus","model":"PHK110","product":"PHK110","tags":"release-keys","type":"user","isPhysicalDevice":true,"androidId":"a6763bf2f12cef23","bundleID":"pop.squid.streaming"}';
-modifiedHeaders['version'] = '0.8.2';
 
 
-modifiedHeaders['system'] = 'Android';
-$done({headers : modifiedHeaders});
 
+
+
+if ($request.url.indexOf("/Account/LoginV2") != -1) {
+    var requestHeaders = $request.headers;
+        
+        requestHeaders['device'] = '{"board":"taro","bootloader":"unknown","brand":"OnePlus","device":"OP5913L1","display":"PHK110_11_A.23","fingerprint":"OnePlus/PHK110/OP5913L1:13/SKQ1.221012.001/T.119565e-24c-14506e:user/release-keys","hardware":"qcom","host":"dg02-pool03-kvm97","id":"SKQ1.221012.001","manufacturer":"OnePlus","model":"PHK110","product":"PHK110","tags":"release-keys","type":"user","isPhysicalDevice":true,"androidId":"a6763bf2f12cef23","bundleID":"pop.squid.streaming"}';
+        requestHeaders['version'] = '0.8.2';
+        requestHeaders['system'] = 'Android';
+    $done({
+        "headers": requestHeaders
+    });
+}

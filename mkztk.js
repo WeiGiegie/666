dd15c9430d6e74ec2e25e5fd6d3e4c7390682a9b
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-03-13 13:37:09 +0800

    更新 mkztk.js

--- mkztk.js
+++ mkztk.js
@@ -23,9 +23,34 @@ hostname = member.mkzcdn.com,comic.mkzcdn.com,base.mkzcdn.com
 */
 
 
+/*
+ *
+ *
+脚本功能：漫客宅,会员漫画小说解锁
+软件版本：++
+下载地址：
+脚本作者：
+更新时间：2025年3月13日 13:36
+电报频道：https://t.me/GieGie777
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# > 漫客宅,会员解锁
+^https?:\/\/(member|comic|base).mkzcdn.com\/(user|chapter\/buy\/add|vote\/add|gift\/play\/add|advert\/app\/(read|user|task)).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/mankezhan.js
+^https?:\/\/(comic|story).mkzcdn.com\/chapter\/(content\/v1|read\/check).*$ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mkztk.js
+[mitm] 
+hostname = member.mkzcdn.com,comic.mkzcdn.com,base.mkzcdn.com
+
+
+*
+*
+*/
+
+
 var modifiedUrl = $request.url
-.replace(/(&sign=\w+)/, '&sign=24e80e21cfc1dff2491c06723978b569')
-.replace(/(&uid=\d+)/, '&uid=77953602');
+.replace(/(&sign=\w+)/, '&sign=f9a09101863726eaadf88a7a1a13df27')
+.replace(/(&uid=\d+)/, '&uid=77979196');
 
 
 $done({

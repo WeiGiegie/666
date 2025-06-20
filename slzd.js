73b07247aa23d0e0cf55de3c64c90c8e4fc37bd0
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-06-20 13:12:34 +0800

    更新 slzd.js

--- slzd.js
+++ slzd.js
@@ -5,7 +5,7 @@
 软件版本：++
 下载地址：苹果商店下载
 脚本作者：
-更新时间：2025年6月11日 08:48
+更新时间：2025年6月20日 13:11
 电报频道：https://t.me/GieGie777
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
@@ -34,17 +34,17 @@ if (url.indexOf(p1) != -1) {
         },
         "model": {
             "msgRed": 0,
-            "registerTime": 1749602913178,
+            "registerTime": 1750396317470,
             "arOpen": true,
             "visitor": {
-                "uid": 2938269,
-                "ticket": "1749602913210vftxc"
+                "uid": 2941160,
+                "ticket": "1750396317501jnjdd"
             },
             "buyZhuanlanNewArticle": 1,
             "buyMsgNum": 1,
             "userInfo": {
                 "phone": "",
-                "uid": 2938269,
+                "uid": 2941160,
                 "userVflag": 0,
                 "remark": "此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖!!!作者TG频道 : https://t.me/GieGie777",
                 "pic": "https://zdimg.lifeweek.com.cn/app/20230410/16811146599505136.jpg",

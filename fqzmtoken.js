95c4bb75dc06e56b8c811ee93f2dad1ac35d4978
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-09-18 15:06:16 +0800

    自动更新Token - 2025-09-18T07:06:15.375Z

--- fqzmtoken.js
+++ fqzmtoken.js
@@ -1,1 +1,7 @@
-hello
+// ëV©¡ú¨÷ö„Token
+// ÷÷öô: 2025-09-18T07:06:15.375Z
+//°ÿ±: ripifo7774@walakato.com
+//{Æ: 33583211
+
+const token = "Xfo4kl6dFbbZRa-fDPxGkyknfHD_A_NU_1758179175";
+module.exports = token;

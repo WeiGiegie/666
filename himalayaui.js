8fcdf3c03b224e9e477ae862635bd081e487da8d
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-09-25 21:00:53 +0800

    更新 himalayaui.js

--- himalayaui.js
+++ himalayaui.js
@@ -16,7 +16,7 @@
 ^https?:\/\/www\.himalaya\.com\/courses\/\d+ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/himalayaui.js
 
 [mitm]
-hostname = www.himalaya.com,apisg.himalaya.com,183.232.157.73:13037
+hostname = www.himalaya.com,apisg.himalaya.com,183.232.157.73:13037,cdn.himalaya.com:443,*.himalaya.*
 *
 *
 */

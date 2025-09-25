82711454d781c976f8823c9c218c949e2410ad57
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-09-26 00:45:25 +0800

    更新 bdyybata1.js

--- bdyybata1.js
+++ bdyybata1.js
@@ -1,6 +1,6 @@
 {
   "vipendtime": "2025-09-26 03:00:03",
-  "cookie": "user_type=0; 1&_device=iPhone&a30b491e-7ec7-4d3f-8021-50f605062f50&2.25.1; x-intl-mulehorse-bucketIds=%2C; net-mode=WIFI; channel=ios-appstore; country_id=1; idfa=d33184c1-413a-4bb0-8c3a-76639baa0842; domain=apisg.himalaya.com; appsflyer_id=1758816001-1111111; locale=zh_CN; path=/; 1&_token=10006960024&28b9711c00efe04c710b2280b289731fd44c; location=sg; impl=com.ximalaya.xmlyi; x-abtest-bucketIds=; JSESSIONID=",
+  "cookie": "user_type=0; 1&_device=iPhone&a30b491e-7ec7-4d3f-8021-50f605062f50&2.25.1; x-intl-mulehorse-bucketIds=%2C; net-mode=WIFI; channel=ios-appstore; country_id=1; idfa=d33184c1-413a-4bb0-8c3a-76639baa0842; domain=api.himalaya.com; appsflyer_id=1758816001-1111111; locale=zh_CN; path=/; 1&_token=10006960024&28b9711c00efe04c710b2280b289731fd44c; location=sg; impl=com.ximalaya.xmlyi; x-abtest-bucketIds=; JSESSIONID=",
   "email": "681785618@gmail.com",
   "uid": 10006960024,
   "token": "28b9711c00efe04c710b2280b289731fd44c",

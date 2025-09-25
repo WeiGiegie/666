9826aa222744c44e47fda109a31a7e8ae9de6809
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-09-26 01:00:06 +0800

    更新 bdyybata1.js

--- bdyybata1.js
+++ bdyybata1.js
@@ -1,6 +1,6 @@
 {
-  "vipendtime": "2025-09-26 03:00:03",
-  "cookie": "user_type=; 1&_device=iPhone&a30b491e-7ec7-4d3f-8021-50f605062f50&2.25.1; x-intl-mulehorse-bucketIds=%2C; net-mode=WIFI; channel=ios-appstore; country_id=1; idfa=d33184c1-413a-4bb0-8c3a-76639baa0842; domain=api.himalaya.com; appsflyer_id=1758816001-1111111; locale=zh_CN; path=/; 1&_token=10006960024&28b9711c00efe04c710b2280b289731fd44c; location=; impl=com.ximalaya.xmlyi; x-abtest-bucketIds=; JSESSIONID=",
+  "vipendtime": "2222-09-26 03:00:03",
+  "cookie": "_ga_K7Z04BZMT9=GS2.1.s1758817303$o1$g1$t1758817353$j10$l0$h0; ajs_anonymous_id=87df65f5-9dcc-4723-80dc-0e063a290d4f; ajs_user_id=6866095; _fbp=fb.1.1758817303974.169446115473286813; _ga=GA1.1.1171711301.1758817303; _gid=GA1.2.102343586.1758817303; 1&_token=10006960024&28b9711c00efe04c710b2280b289731fd44c; 1&remember_me=y; 1_l_flag=6866095&86f6d1a20634d040150947304b1817c82cc1_1758817339342; _gat_gtag_UA_119165824_3=1; countryCode=tw; countryId=14; country_id=14; country_tsp=202108261605; i18n_tsp=202302071642; locale=zh_TW; location=us",
   "email": "681785618@gmail.com",
   "uid": 10006960024,
   "token": "28b9711c00efe04c710b2280b289731fd44c",

72eab54a6918785ff541a58f7ef47835f6a41c60
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-09-26 23:03:49 +0800

    更新 himalayaui.js

--- himalayaui.js
+++ himalayaui.js
@@ -12,6 +12,7 @@
 *******************************
 [rewrite_local]
 # > himalaya vip 
+^https?:\/\/api.himalaya.com\/himalaya-portal\/profile\/get\/current\/user\/channels url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/himalayack.js
 ^https?:\/\/api.himalaya.com\/himalaya-portal\/v2\/vip\/premium\/ url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/himalayack.js
 ^https?:\/\/apisg.himalaya.com\/himalaya-portal\/(track\/urlEncode\?trackId|v1\/course\?courseId) url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/himalayack.js 
 ^https?:\/\/www\.himalaya\.com\/courses\/\d+ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/himalayaui.js

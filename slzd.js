ede160acae832746a00aff28f7a55c2ba8781c99
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-10-03 17:30:15 +0800

    更新 slzd.js

--- slzd.js
+++ slzd.js
@@ -1,109 +1,67 @@
 /*
  *
  *
-脚本功能：三联中读,知识会员+数字刊会员
-软件版本：++
-下载地址：苹果商店下载
+脚本功能：三联中读  (选择设备登录，会员到期后再重新登录即可)
+软件版本：10.7.3
+下载地址：
 脚本作者：
-更新时间：2025年10月1日 17:45
+更新时间：2025年
 电报频道：https://t.me/GieGie777
 问题反馈：
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 [rewrite_local]
-# > 三联中读,知识会员+数字刊会员
-^https?:\/\/apis\.lifeweek\.com\.cn\/(vip\/loadMyVipV2\?|index\/home.do\?).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/slzd.js
+# > 三联中读 知识会员，数字刊会员
+^https?:\/\/apis.lifeweek.com.cn\/reg\/vistorRegister.do url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/slzd.js
 
-[mitm] 
+
+[mitm]
 hostname = apis.lifeweek.com.cn
 *
 *
 */
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const p1 = "/index/home.do?";
-const p2 = "/vip/loadMyVipV2?";
 
 
-if (url.indexOf(p1) != -1) {
-    obj = {
-        "extraInfoMap": {
 
-        },
-        "model": {
-            "msgRed": 0,
-            "registerTime": 1759311906903,
-            "arOpen": true,
-            "visitor": {
-                "uid": 2981598,
-                "ticket": "1759311906940dujge"
-            },
-            "buyZhuanlanNewArticle": 1,
-            "buyMsgNum": 1,
-            "userInfo": {
-                "phone": "",
-                "uid": 2981598,
-                "userVflag": 0,
-                "remark": "此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖!!!作者TG频道 : https://t.me/GieGie777",
-                "pic": "https://zdimg.lifeweek.com.cn/app/20230410/16811146599505136.jpg",
-                "isVip": true,
-                "name": "https://t.me/GieGie777"
-            }
-        },
-        "resultMsg": "操作成功！",
-        "redirectUrl": null,
-        "resultCodeBean": "SUCCESS",
-        "resultCode": "SUCCESS",
-        "success": true,
-        "pickParams": {
 
+const remoteUrl = "https://raw.githubusercontent.com/WeiGiegie/666/main/slzdtoken.json";
+
+(async () => {
+    try {
+        const { response, body } = await fetchRemoteContent({ url: remoteUrl });
+        const statusCode = response.status || response.statusCode;
+
+        if (statusCode !== 200) {
+            return $done({});
         }
+
+        JSON.parse(body);
+
+        let newHeaders = $response.headers;
+        const contentTypeKey = Object.keys(newHeaders).find(key => key.toLowerCase() === 'content-type') || 'Content-Type';
+        newHeaders[contentTypeKey] = 'application/json; charset=utf-8';
+
+        $done({ body, headers: newHeaders });
+
+    } catch (error) {
+        $done({});
     }
+})();
 
-
-    body = JSON.stringify(obj);
-}
-if (url.indexOf(p2) != -1) {
-    obj = {
-        "extraInfoMap": {
-
-        },
-        "model": [{
-            "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
-            "vipLevel": 4,
-            "vipExpiration": "2999-09-09",
-            "vip": true,
-            "username": "https://t.me/GieGie777",
-            "vipValid": true
-        }, {
-            "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
-            "vipLevel": 5,
-            "vipExpiration": "",
-            "vip": false,
-            "username": "https://t.me/GieGie777",
-            "vipValid": true
-        }, {
-            "userHeadPic": "https://zdimg.lifeweek.com.cn/app/20230410/16810960185662892.jpg",
-            "vipLevel": 6,
-            "vipExpiration": "2999-09-09",
-            "vip": true,
-            "username": "https://t.me/GieGie777",
-            "vipValid": true
-        }],
-        "resultMsg": "操作成功！",
-        "redirectUrl": null,
-        "resultCodeBean": "SUCCESS",
-        "resultCode": "SUCCESS",
-        "success": true,
-        "pickParams": {
-
-        }
+function fetchRemoteContent(options) {
+  return new Promise((resolve, reject) => {
+    if (typeof $httpClient !== 'undefined') {
+      $httpClient.get(options, (error, response, body) => {
+        if (error) return reject(error);
+        resolve({ response, body });
+      });
+    } else if (typeof $task !== 'undefined') {
+      $task.fetch(options).then(
+        (response) => resolve({ response, body: response.body }),
+        (reason) => reject(reason.error || reason)
+      );
+    } else {
+      reject("Unsupported environment");
     }
-    body = JSON.stringify(obj);
+  });
 }
-
-$done({
-    body
-});

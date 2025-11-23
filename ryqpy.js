2dd17544bc487c304602843e039fa60d405b97c2
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2025-11-23 21:38:15 +0800

    更新 ryqpy.js

--- ryqpy.js
+++ ryqpy.js
@@ -28,46 +28,12 @@ hostname = apis40.qupeiyin.com
 
 
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+var obj = JSON.parse($response.body);
 
-const courseDetail = '/course/detail_new';
-const memberIndex = '/member/index';
-const courseRelated = '/course/related_course';
-const albumCourse = '/album/course_list';
-
-// 处理课程详情接口
-if (url.indexOf(courseDetail) != -1) {
-    if (obj.data) {
-        obj.data.is_needbuy = "0";
-        obj.data.is_vip = "0";
-    }
-    body = JSON.stringify(obj);
-} 
-
-// 处理会员信息接口
-else if (url.indexOf(memberIndex) != -1) {
-    if (obj.data) {
-        obj.data.is_vip = "1";
-        obj.data.vip_endtime = "4102444800";
-        obj.data.is_svip = "1";
-        obj.data.svip_endtime = "4102444800";
-        obj.data.is_aisvip = "1";
-        obj.data.aisvip_endtime = "4102444800";
-    }
-    body = JSON.stringify(obj);
-} 
-
-// 处理课程列表接口
-else if (url.indexOf(courseRelated) != -1 || url.indexOf(albumCourse) != -1) {
-    if (obj.data && Array.isArray(obj.data)) {
-        obj.data.forEach(video => {
-            video.is_needbuy = "0";
-            video.is_vip = "0";
-        });
-    }
-    body = JSON.stringify(obj);
+if (obj.data) {
+    obj.data.is_vip = "1";   
+    obj.data.type = "2";     
+    
 }
 
-$done({body});
+$done({body: JSON.stringify(obj)});

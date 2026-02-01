321ae56fa449a55ffcf563fad5b32967a734c48f
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2026-02-02 02:02:33 +0800

    更新 bijia.js

--- bijia.js
+++ bijia.js
@@ -7,7 +7,7 @@
 脚本作者：#https://raw.githubusercontent.com/mw418/Loon/main/script/jd_price.js#
 更新时间：2025年
 电报频道：https://t.me/GieGie777
-问题反馈：
+问题反馈：无需第三方软件获取Cookie、历史价格会显示在京东商品“详情”页。
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 [rewrite_local]

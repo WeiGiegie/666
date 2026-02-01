132df9459d35d812a33f19db45dcec23c0e70267
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2026-02-02 02:02:55 +0800

    更新 bijia.js

--- bijia.js
+++ bijia.js
@@ -5,7 +5,7 @@
 软件版本：15.3.30
 下载地址：
 脚本作者：#https://raw.githubusercontent.com/mw418/Loon/main/script/jd_price.js#
-更新时间：2025年
+更新时间：2026年
 电报频道：https://t.me/GieGie777
 问题反馈：无需第三方软件获取Cookie、历史价格会显示在京东商品“详情”页。
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！

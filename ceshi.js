f5ea2d4d157cb8729d6b0ccbbcd83d770bf0e730
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2026-02-09 19:30:14 +0800

    喜马拉雅自主注册2026年2月9日 19:30

--- ceshi.js
+++ ceshi.js
@@ -1,12 +1,5 @@
 {
-  "fingerprint": "45836eb399dc2cf82b8974f68ef90de4",
-  "vip_expiry": "2026-02-14T17:31:55.000Z",
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEzNiwidXNlcm5hbWUiOiJHMTU2NTQ1NjQ1NiIsInBob25lIjpudWxsLCJlbWFpbCI6bnVsbCwicmVmZXJyYWxfY29kZSI6IjM3Mzk0YyIsInJlZmVycmVkX2J5IjpudWxsLCJ2aXBfZXhwaXJ5IjoiMjAyNi0wMi0xNFQxNzozMTo1NS4wMDBaIiwiaW52aXRlZF9jb3VudCI6MTcsImZpbmdlcnByaW50IjoiNDU4MzZlYjM5OWRjMmNmODJiODk3NGY2OGVmOTBkZTQiLCJpcF9hZGRyZXNzIjoiMTMuMjEzLjUwLjIxNCIsImxhc3RfbG9naW4iOiIyMDI2LTAyLTA5VDEwOjQwOjE4LjAwMFoiLCJjcmVhdGVkX2F0IjoiMjAyNi0wMS0yM1QwOTozMzozNy4wMDBaIiwidXBkYXRlZF9hdCI6IjIwMjYtMDItMDlUMTA6NDA6MTguMDAwWiIsImlhdCI6MTc3MDYzNDUwOSwiZXhwIjoxNzcxMjM5MzA5fQ.ElC5t6Cv6I5-gm9DvBZDYlraxA9d29ObkXAqjpcQi1c",
-  "username": "G1565456456",
-  "referral_code": "37394c",
-  "invited_count": 17,
-  "last_login": "2026-02-09T10:40:18.000Z",
-  "upload_time": "2026-02-09T10:55:09.436Z",
-  "update_time": "2026-02-09T10:55:11.673Z",
-  "note": "糖心刷邀请自动更新脚本生成"
+  "vipendtime": "测试",
+  "cookie": "domain=.ximalaya.com; path=/; channel=ios-b1; impl=com.ximalaya.tingLite; 1&_device=iPhone&901033FB-GGG1-11AA-1999-B1450B901033&3.0.34;1&_token=513689497&E5911350240C80561B7BB532D9EDEE3C11D24C2EB902CEEA309768378929346BF343A0A40DCC126MEB5F1537FA4B5CA_"
+
 }

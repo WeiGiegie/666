5f5c8d8ae65f2273e551176940aa39aeb846a055
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2026-03-10 11:01:41 +0800

    更新 tmmyxpk.js

--- tmmyxpk.js
+++ tmmyxpk.js
@@ -11,8 +11,7 @@
 使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
 *******************************
 [rewrite_local]
-^https?:\/\/h5-tthd-server.jinkejoy.com\/user\/login url script-response-body https://raw.githubusercontent.com/WeiGiegie666/main/tmmyxpk.js
-
+^https?:\/\/h5-tthd-server.jinkejoy.com\/user\/login url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/tmmyxpk.js
 [mitm]
 hostname = h5-tthd-server.jinkejoy.com
 *

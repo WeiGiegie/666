e3e05d778f7790f1e82011807a60472a1d89ada2
WeiGiegie <158291476+WeiGiegie@users.noreply.github.com>
2026-03-12 19:04:30 +0800

    创建 mhlz.js

---
+++ mhlz.js
@@ -0,0 +1,51 @@
+/*
+脚本功能：魔幻粒子 - 游戏货币修改
+软件版本：
+下载地址：
+脚本作者：
+更新时间：
+电报频道：https://t.me/GieGie777
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+^https?:\/\/ss\.landintheair\.com\/storage\/ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/mhlz.js
+
+[mitm]
+hostname = ss.landintheair.com
+*
+*
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var body = $response.body;
+let obj = JSON.parse(body);
+
+// 修改所有货币的持有量和累计收集量（最大数值）
+if (obj && obj.currencies && obj.currencies.list) {
+    for (let key in obj.currencies.list) {
+        let currency = obj.currencies.list[key];
+        if (currency.amount !== undefined) {
+            currency.amount = "9999999999999988888888";      // 当前持有量
+        }
+        if (currency.total_collected !== undefined) {
+            currency.total_collected = "9999999999999988888888"; // 总收集量
+        }
+       
+    }
+}
+
+
+$done({ body: JSON.stringify(obj) });
